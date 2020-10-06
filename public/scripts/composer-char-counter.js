8ba1c1e623cc7809a390087bc983a35dfb4809c3
helenohyeah <ouyang.helen@gmail.com>
2020-10-06 20:14:58 +0000

    Add character counter functionality and styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,28 @@
-console.log('i\'m here!');
+/*
+ * Character counter for new tweet composer
+ * Changes the character count based on the tweet's contents
+ *
+ */
+
+ $(document).ready(function() {
+  
+  $('textarea#tweet-text').on('keyup', function(e) {
+    const maxChar = 140;
+    const currChar = $(this).val().length;
+    const charCount = maxChar - currChar;
+    
+    // select the character counter
+    const counter = $(this).next().children('.counter');
+
+    // change counter color
+    if (charCount > 0) {
+      counter.removeClass('invalid');
+    }
+    if (charCount < 0) {
+      counter.addClass('invalid');
+    }
+
+    // updates counter value
+    counter.text(charCount);
+  });
+ });
