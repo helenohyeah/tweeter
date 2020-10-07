b34ef94ad21477b7552ad8480fa06dc2a571f003
helenohyeah <ouyang.helen@gmail.com>
2020-10-07 15:43:33 +0000

    Show Error for invalid tweet length

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,10 +6,54 @@
 
 $(document).ready(function() {
 
+  const $form = $('.new-tweet form');
+  const $textarea = $('#tweet-text');
+
+  // submit handler to post a new tweet asynchronously
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    const msg = $textarea.val();
+    console.log('msg>>', msg);
+    console.log('msg.length>>', msg.length);
+    
+    // error handling
+    if (msg.length === 0) {
+      alert('Error: Nothing in tweet')
+    } else if (msg.length > 140) {
+      // this tweet is exactly exactly exactly exactly exactly exactly exactly exactly exactly exactly  exactly exactly exactly exactly 141 characters
+      alert('Error: Tweet exceeds 140 characcters');
+     
+    // no errors => send post request
+    } else {
+      const data = $(this).serialize();
+      $.ajax('/tweets', { method: 'POST', data })
+        .then(function() {
+          // clears the form
+          $textarea.val('');
+        })
+        // .then(function () {
+        //   $.ajax('/tweets', { method: 'GET' })
+        // }).then(function (tweets) {
+        //   renderTweets(tweets);
+        // });
+    }
+
+  });
+
+  // requests array of tweets
+  const loadTweets = () => {
+    $.ajax('/tweets', { method: 'GET' })
+      .then(function (tweets) {
+        renderTweets(tweets);
+      })
+  };
+
+  loadTweets();
+
   // takes in a tweet object and returns HTML structure
   const createTweetElement = (tweet) => {
 
-    const getTimeSince = (days) => days < 1 ? `${days} day ago` : `${days} days ago`;
+    const getTimeSince = (days) => days === 1 ? `${days} day ago` : `${days} days ago`;
 
     // turns timestamp into X days ago
     const today = Date.now();
@@ -49,30 +93,30 @@ $(document).ready(function() {
     });
   };
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
+  // const data = [
+  //   {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png"
+  //       ,
+  //       "handle": "@SirIsaac"
+  //     },
+  //     "content": {
+  //       "text": "If I have seen further it is by standing on the shoulders of giants"
+  //     },
+  //     "created_at": 1461116232227
+  //   },
+  //   {
+  //     "user": {
+  //       "name": "Descartes",
+  //       "avatars": "https://i.imgur.com/nlhLi3I.png",
+  //       "handle": "@rd" },
+  //     "content": {
+  //       "text": "Je pense , donc je suis"
+  //     },
+  //     "created_at": 1461113959088
+  //   }
+  // ];
   
-  renderTweets(data);
+  // renderTweets(data);
 });
